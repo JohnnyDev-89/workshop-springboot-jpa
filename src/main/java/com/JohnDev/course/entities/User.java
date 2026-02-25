dc79e57037bd2ca05fd84dfce574e137fb9a80d7
JohnnyDev-89 <jgusalmeida@gmail.com>
2026-02-25 16:52:24 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/JohnDev/course/entities/User.java
+++ src/main/java/com/JohnDev/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.JohnDev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +27,16 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	
+	//Em associações Muitos para um, quando é carregado um objeto do lado MUITOS,
+	//automaticamente o JPA carrega a associação equivalente do lado UM.
+	//O mesmo não acontece quando é carregado um objeto do lado UM.
+	//Bastando colocar o JsonIgnore no lado UM
+	//Lazy Loading
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +89,14 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
--- src/main/java/com/JohnDev/course/services/OrderService.java
+++ src/main/java/com/JohnDev/course/services/OrderService.java
@@ -6,21 +6,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.JohnDev.course.entities.User;
-import com.JohnDev.course.repositories.UserRepository;
+import com.JohnDev.course.entities.Order;
+import com.JohnDev.course.repositories.OrderRepository;
 
 @Service
-public class UserService {
+public class OrderService {
 	
 	@Autowired
-	private UserRepository repository;
+	private OrderRepository repository;
 	
-	public List<User> findAll() {
+	public List<Order> findAll() {
 		return repository.findAll();
 	}
 	
-	public User findById(Long id) {
-		Optional<User> optional = repository.findById(id);
+	public Order findById(Long id) {
+		Optional<Order> optional = repository.findById(id);
 		return optional.get();
 	}
 
